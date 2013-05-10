6abeafff8529d89f390fa8a9410bea61d061158b
theopolisme <theopolismewiki@gmail.com>
2013-05-10 16:31:27 -0500

    Fix donenow/checkpage functionality; trial complete

--- external_link_remover.py
+++ external_link_remover.py
@@ -18,9 +18,9 @@ site.login(password.username, password.password)
 
 try:
 	temp_var = naughty_links[1]
-	summary = "Removing links to defunct sites: {0}. ([[WP:BOT|bot]] on trial - [[User:Theo's Little Bot/disable/external links|disable]])".format(", ".join(naughty_links))
+	summary = "Removing links to defunct sites: {0}. ([[WP:BOT|bot]] - [[User:Theo's Little Bot/disable/external links|disable]])".format(", ".join(naughty_links))
 except IndexError:
-	summary = "Removing links to defunct site, {0}. ([[WP:BOT|bot]] on trial - [[User:Theo's Little Bot/disable/external links|disable]])".format(naughty_links[0])
+	summary = "Removing links to defunct site, {0}. ([[WP:BOT|bot]] - [[User:Theo's Little Bot/disable/external links|disable]])".format(naughty_links[0])
 
 full_results = []
 to_process = []
@@ -32,26 +32,26 @@ for link in naughty_links:
 		full_results.append(item)
 
 print "Generating list of mainspace pages linking to {0}...".format(naughty_links)
-done = 0
+
 for page in full_results:
-	if page[u'ns'] == 0 and page[u'title'] not in to_process and done < 11:
+	if page[u'ns'] == 0 and page[u'title'] not in to_process:
 		print("{0}".format(page[u'title']).encode('UTF-8'))
 		to_process.append(site.Pages[page[u'title']])
-		done += 1
 
 print "Processing mainspace pages linking to {0}...".format(naughty_links)
 
+donenow = 0
 for page in to_process:
-	if donenow("User:Theo's Little Bot/disable/external links",donenow_div=5) == True:
+	if bot.donenow("User:Theo's Little Bot/disable/external links",donenow=donenow,donenow_div=5,shutdown=40) == True:
 		print "~~~\n~~~\n"
 		contents = page.edit()
 		for link in naughty_links:
-			contents = re.sub(r"<ref.*?" + link.replace('.',r'\.') + r".*?</ref>", '', contents, flags=re.UNICODE | re.DOTALL)
+			contents = re.sub(r"<ref[^<]*?" + link.replace('.',r'\.') + r".*?</ref>", '', contents, flags=re.UNICODE | re.DOTALL)
 			contents = re.sub(r"\*(?!.*?<ref).*?" + link.replace('\.',r'\.') + r".*", '', contents, flags=re.UNICODE)
 			contents = re.sub(r"\[.*" + link.replace('.',r'\.') + r".*]", '', contents, flags=re.UNICODE)
-
 		page.save(contents,summary=summary)
+		donenow += 1
 	else:
-		sys.exit('Program was disabled on-wiki.')
+		sys.exit('Program was disabled.')
 
 print "We're done!"
