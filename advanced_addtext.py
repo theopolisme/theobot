e50e50b80403038f1a3897e1eddf1da300ae22ca
theopolisme <theopolismewiki@gmail.com>
2013-07-06 11:30:50 -0500

    expand (now I have a working implementation for categories, at least)

--- advanced_addtext.py
+++ advanced_addtext.py
@@ -2,33 +2,30 @@
 
 from __future__ import unicode_literals
 import sys
-import re
 
 import mwclient
 import mwparserfromhell
 
 from theobot import password
-from theobot import bot
 
 # CC-BY-SA Theopolisme
 
-"""
+ABOUT = """
 
 IN PROGRESS
 
 This script will add text at a given position in a page, based on the
-position parameter, which should be a number cooresponding to an item
+position parameter, which should be a term cooresponding to an item
 in the following list:
 
-1) External links * 
-2) Navigation templates (footer navboxes)
-3) Geographical coordinates (if not in Infobox) or {{coord missing}} *
-4) Authority control template *
-5) Persondata template *
-6) Defaultsort *
-7) Categories
-8) Stub template *
-9) Very bottom
+'external' == External links * 
+'nav' == Navigation templates (footer navboxes)
+'geo' == Geographical coordinates (if not in Infobox) or {{coord missing}} *
+'authcontrol' == Authority control template *
+'persondata' == Persondata template *
+'defaultsort' == Defaultsort *
+'category' == Categories
+'stub' == Stub template *
 
 * not yet implemented
 
@@ -39,7 +36,7 @@ in the following list:
 
 `Text` should be the text that you would like added, and `page_contents` should
 be a string/unicode object that represents the wikitext into which the text will
-be inserted. Position should be an integer from the above list denoting the
+be inserted. Position should be a quoted term from the above list denoting the
 position where you want the text inserted. `add_text()` will return a string/unicode
 object of the new wikitext with the text inserted.
 
@@ -49,7 +46,7 @@ This script can be run on the command line, in the following format:
 	python advanced_addtext.py "text" page -position
 
 For example,
-	python advanced_addtext.py "Hello, World!" Wikipedia -2
+	python advanced_addtext.py "Hello, World!" Wikipedia -nav
 adds the text "Hello, World!" to the article Wikipedia, in the position
 where navigation templates belong.
 
@@ -60,15 +57,53 @@ class ParsedPage(object):
 	access to a specific area in the page (ex. nav templates).
 	"""
 
-	def add_
+	def __init__(self,page_contents):
+		self.wikicode = mwparserfromhell.parse(page_contents)
+		self.parse()
 
+	def parse(self):
+		self.bottom = len(self.wikicode)
+		self.CATEGORIES = []
+		self.CAT_INDEX = None
+		self._cats()
+
+	def _cats(self):
+		for node in self.wikicode.ifilter():
+			if unicode(node.title).find("Category:") != -1:
+				index = self.wikicode.index(node)
+				self.CATEGORIES += node.title
+				if index > self.CAT_INDEX:
+					self.CAT_INDEX = index 
+		if self.CAT_INDEX == None:
+			self.CAT_INDEX = self.bottom # Hacky, just adds cats to the very bottom
+
+	def insert(self,text,position):
+		if position == 'category':
+			self.wikicode.insert(self.CAT_INDEX+1,"\n"+text)
+		else:
+			raise NotImplementedError("""Sorry, but "{}" insertion isn't currently implemented.""".format(position))
+
+		return self.wikicode		
 
 def add_text(text,page_contents,position):
+	parsed = ParsedPage(page_contents)
+	result = parsed.insert(text,position)
+	return result
 
 def main():
-	global site
+	if len(sys.argv) == 4:
+		inserttext = sys.argv[1]
+		pagename = sys.argv[2]
+		type_of_insert = sys.argv[3].strip('-')
+	elif 4 > len(sys.argv) > 1:
+		raise ValueError("""Exactly four inputs, please, like so:\n$ python advanced_addtext.py "text" page -position\n(Run with no args for detailed instructions)""")
+	else:
+		print ABOUT
+		sys.exit()
+
 	site = mwclient.Site('en.wikipedia.org')
-	site.login(password.main_username, password.main_password)
+	page = site.Pages[pagename]
+	print add_text(inserttext,page.edit(),type_of_insert)
 
 if __name__ == '__main__':
 	main()
