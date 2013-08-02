44eff8664abdfe085b6bd61a858f1a015dd70adc
theopolisme <theopolismewiki@gmail.com>
2013-08-02 11:57:38 -0500

    Generalize script; allow command line entry of template and parameter to process

--- startdate.py
+++ startdate.py
@@ -1,16 +1,21 @@
 #! /usr/bin/env python
+"""
+Converts a given parameter in a template to use {{start date}}
+$ python startdate.py name_of_infobox_to_convert name_of_param_to_convert
+"""
+
 from __future__ import unicode_literals
 
 import mwclient
 import mwparserfromhell
 
+import sys
 import re
 
 from theobot import bot
 from theobot import password
 
 from datetime import datetime
-
 import dateutil.parser as parser
 
 # CC-BY-SA Theopolisme
@@ -24,21 +29,21 @@ def process(page):
 	contents = page.edit()
 	wikicode = mwparserfromhell.parse(contents)
 	for template in wikicode.filter_templates():
-		if "infobox poem" in template.name.lower() and template.has_param('publication_date'):
-			pub_date_stripped = template.get('publication_date').value.strip_code().strip() # This helps with parsing wikicode-ified dates
-			pub_date_raw = template.get('publication_date').value.strip()
-			print "-----"
-			print "Raw publication_date: "+pub_date_raw
+		if template.name.lower().strip() in INFOBOX_TITLES and template.has_param(PARAM):
+			pub_date_stripped = template.get(PARAM).value.strip_code().strip() # This helps with parsing wikicode-ified dates
+			pub_date_raw = template.get(PARAM).value.strip()
+			#print "-----"
+			#print "Raw {}: ".format(PARAM)+pub_date_raw
 			if pub_date_raw.lower().find("{{start") == -1 and pub_date_raw.find("[[Category:Infoboxes needing manual conversion") == -1:
 				date = parser.parser().parse(pub_date_stripped,None)
 				if date is None or date.year is None:
 					if pub_date_raw.find("<!-- Date published") == -1:
-						template.add('publication_date',pub_date_raw+" [[Category:Infoboxes needing manual conversion to use start date]]")
-						page.save(unicode(wikicode),'[[WP:BOT|Bot]]: Tagging unparsable publication_date')
-						print "Tagging unparsable publication_date"
+						template.add(PARAM,pub_date_raw+" [[Category:Infoboxes needing manual conversion to use start date]]")
+						page.save(unicode(wikicode),u'[[WP:BOT|Bot]]: Tagging unparsable [[Template:{}]]'.format(PARAM))
+						#print "Tagging unparsable {}".format(PARAM)
 						continue
 					else:
-						print "No publication date specificed; skipping."
+						#print "No {} specificed; skipping.".format(PARAM)
 						continue					
 				if re.search(r"""\d{1,2} [a-zA-Z]* \d{4}""",pub_date_raw,flags=re.U) is not None:
 					df = True
@@ -53,11 +58,11 @@ def process(page):
 					startdate.add(3,date.day)
 				if df:
 					startdate.add('df','y')
-				print "Final template: "+unicode(startdate)
-				template.add('publication_date',unicode(startdate)+"<!-- Bot-converted date -->")
-				page.save(unicode(wikicode),'[[WP:BOT|Bot]]: Converting publication_date to utilize {{[[Template:start date|]]}}')
+				#print "Final template: "+unicode(startdate)
+				template.add(PARAM,unicode(startdate)+"<!-- Bot-converted date -->")
+				page.save(unicode(wikicode),u'[[WP:BOT|Bot]]: Converting '+PARAM+' to utilize {{[[Template:start date|]]}}')
 			else:
-				print "Template already updated."
+				#print "Template already updated."
 				continue
 
 def main():
@@ -65,7 +70,15 @@ def main():
 	site = mwclient.Site('en.wikipedia.org')
 	site.login(password.username, password.password)
 
-	for page in bot.what_transcludes('infobox poem'):
+	print "Logged in to site; getting template details."
+	global INFOBOX,PARAM,INFOBOX_TITLES
+	INFOBOX = sys.argv[1].replace('_',' ')
+	PARAM = sys.argv[2]
+	INFOBOX_TITLES = [title.strip().lower() for title in bot.redirects(INFOBOX,namespace=10,pg_prefix="Template:",output='page_title')]
+	INFOBOX_TITLES += INFOBOX
+
+	print "Getting transclusions..."
+	for page in bot.what_transcludes(INFOBOX):
 		page = site.Pages[page['title']]
 		process(page)
 
