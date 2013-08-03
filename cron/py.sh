69df145a948bd4658bc9993f9a0a487ea7bfc611
theopolisme <theopolismewiki@gmail.com>
2013-08-02 19:58:40 -0500

    I don't use these scripts on labs

--- cron/py.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# run an arbitrary Python script
-PYTHONPATH=/home/theo/tools:/opt/ts/python/2.7/lib/python2.7/site-packages:/opt/ts/python/2.7/lib/python2.7:/opt/ts/python/2.7/lib/python2.7/plat-sunos5:/opt/ts/python/2.7/lib/python2.7/lib-tk:/opt/ts/python/2.7/lib/python2.7/lib-old:/opt/ts/python/2.7/lib/python2.7/lib-dynload:/opt/ts/python/2.7/lib/python2.7/site-packages:/opt/ts/python/2.7/lib/python2.7/site-packages/PIL
-export PYTHONPATH
-/usr/bin/python $@
-
